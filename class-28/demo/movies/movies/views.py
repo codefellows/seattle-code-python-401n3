7cbf68c1a71c548b14b7cb1f3bc65dbfa1de335d
Roger Huba <rogerhuba@gmail.com>
2021-08-28 14:30:46 -0500

    add demo

--- class-28/demo/movies/movies/views.py
+++ class-28/demo/movies/movies/views.py
@@ -1,28 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Movie
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 class MovieListView(ListView):
-    template_name = 'movie_list.html'
+    template_name = 'movie-list.html'
     model = Movie
 
 class MovieDetailView(DetailView):
-    template_name = 'movie_detail.html'
+    template_name = 'movie-detail.html'
     model = Movie
 
 class MovieCreateView(CreateView):
-    template_name = 'movie_create.html'
+    template_name = 'movie-create.html'
     model = Movie
     fields = ['name', 'description', 'owner']
-    success_url = reverse_lazy('list_view')
 
 class MovieUpdateView(UpdateView):
-    template_name = 'movie_update.html'
+    template_name = 'movie-update.html'
     model = Movie
     fields = ['name', 'description', 'owner']
 
 class MovieDeleteView(DeleteView):
-    template_name = 'movie_delete.html'
+    template_name = 'movie-delete.html'
     model = Movie
-    success_url = reverse_lazy('list_view')
+    success_url = reverse_lazy('movie_list')
