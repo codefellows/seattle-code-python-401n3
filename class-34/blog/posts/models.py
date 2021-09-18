5b023e7882d68afa667e58787bb515302785d4e6
Roger Huba <rogerhuba@gmail.com>
2021-09-18 13:50:06 -0500

    add demo

---
+++ class-34/blog/posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    body = models.TextField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
+
